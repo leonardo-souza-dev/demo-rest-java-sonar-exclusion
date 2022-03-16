a6b0677f6aa27817c753471a2a9c68f349357fad
Leonardo Souza <ltreze@icloud.com>
2022-03-16 16:21:41 -0300

    first commit

---
+++ src/main/java/com/example/demo/controller/ProdutoController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProdutoController {
+
+	@GetMapping("/inserirProduto")
+	public String inserirProduto(@RequestParam(value = "nome") final String nome) throws Exception {
+		return nome;
+	}
+}
